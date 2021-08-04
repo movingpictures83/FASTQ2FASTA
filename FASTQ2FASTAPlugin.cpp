122f456b1e7a8e438f2b766c2c8bda8064aebc1d
Trevor Cickovski <tcickovs@delphi.cs.fiu.edu>
2021-08-04 17:37:21 -0400

    Added the ability to convert multiple FASTQ files

--- FASTQ2FASTAPlugin.cpp
+++ FASTQ2FASTAPlugin.cpp
@@ -3,8 +3,31 @@
 #include <stdlib.h>
 #include "FASTQ2FASTAPlugin.h"
 
+bool fastqFile(std::string file) {
+        int len = file.size();
+     	return (file[len-5] == 'f' &&
+			file[len-4] == 'a' &&
+			file[len-3] == 's' &&
+			file[len-2] == 't' &&
+			file[len-1] == 'q');
+}
+
 void FASTQ2FASTAPlugin::input(std::string file) {
  inputfile = file;
+ if (fastqFile(inputfile)){
+    mode = "single";
+ }
+ else {
+    mode = "multiple";
+    std::ifstream infile(file.c_str(), std::ios::in);
+    std::string singlefile, oldsingle;
+    while (!infile.eof()) {
+       infile >> singlefile;
+       if (singlefile.size() != 0 && singlefile != oldsingle)
+       fastqFiles.push_back(singlefile);
+       oldsingle = singlefile;
+    }
+ }
 }
 
 void FASTQ2FASTAPlugin::run() {}
@@ -12,6 +35,7 @@ void FASTQ2FASTAPlugin::run() {}
 void FASTQ2FASTAPlugin::output(std::string file) {
  std::string outputfile = file;
  std::string myCommand = "";
+ if (mode == "single") {
 myCommand += "bioconvert";
 myCommand += " ";
 myCommand += "fastq2fasta";
@@ -20,6 +44,15 @@ myCommand += inputfile + " ";
 myCommand += outputfile + " ";
 myCommand += "--force";
 myCommand += " ";
+ }
+ else {
+    myCommand += "fq2fa ";
+    myCommand += "--merge --filter ";
+    for (int i = 0; i < fastqFiles.size(); i++)
+       myCommand += std::string(PluginManager::prefix()) + "/" + fastqFiles[i] + " ";
+    myCommand += outputfile;
+ }
+ std::cout << myCommand << std::endl;
  system(myCommand.c_str());
 }
 
