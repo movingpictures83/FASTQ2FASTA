81e495edf343cb0fea54ea8182f15d9ccb4942ea
Trevor Cickovski <tcickovs@delphi.cs.fiu.edu>
2023-07-28 15:56:15 -0400

    Moved to fastq2fasta

--- FASTQ2FASTAPlugin.cpp
+++ FASTQ2FASTAPlugin.cpp
@@ -36,24 +36,31 @@ void FASTQ2FASTAPlugin::output(std::string file) {
  std::string outputfile = file;
  std::string myCommand = "";
  if (mode == "single") {
-myCommand += "bioconvert";
-myCommand += " ";
-myCommand += "fastq2fasta";
-myCommand += " ";
-myCommand += inputfile + " ";
-myCommand += outputfile + " ";
-myCommand += "--force";
-myCommand += " ";
+/*#myCommand += "bioconvert";
+#myCommand += " ";
+#myCommand += "fastq2fasta";
+#myCommand += " ";
+#myCommand += inputfile + " ";
+#myCommand += outputfile + " ";
+#myCommand += "--force";
+#myCommand += " ";
+*/
+myCommand += "fastq2fasta.py ";
+myCommand += inputfile + " >& ";
+myCommand += file;
+std::cout << myCommand << std::endl;
+ system(myCommand.c_str());
  }
  else {
-    myCommand += "fq2fa ";
-    myCommand += "--merge --filter ";
-    for (int i = 0; i < fastqFiles.size(); i++)
-       myCommand += std::string(PluginManager::prefix()) + "/" + fastqFiles[i] + " ";
-    myCommand += outputfile;
- }
+    for (int i = 0; i < fastqFiles.size(); i += 2) {
+       myCommand = "fq2fa ";
+       myCommand += "--merge ";//--filter ";
+       myCommand += std::string(PluginManager::prefix()) + "/" + fastqFiles[i] + " " + std::string(PluginManager::prefix()) + "/" + fastqFiles[i+1] + " ";
+    myCommand += outputfile+"."+fastqFiles[i].substr(fastqFiles[i].find_last_of('/')+1)+".fasta";
  std::cout << myCommand << std::endl;
  system(myCommand.c_str());
+    }
+ }
 }
 
 PluginProxy<FASTQ2FASTAPlugin> FASTQ2FASTAPluginProxy = PluginProxy<FASTQ2FASTAPlugin>("FASTQ2FASTA", PluginManager::getInstance());
